50dcc4e4e1a9bc86a0aa034ded8ef228511adacb
Your Name <edu.fernandes@ua.pt>
2022-03-25 23:33:22 +0000

    done

--- POO/src/Aula03/Ex6.java
+++ POO/src/Aula03/Ex6.java
@@ -8,7 +8,7 @@ public class Ex6 {
 		int ano = Helpers.readInteger("Ano: ");
 
 		int bissexto = 0;
-		if( (ano % 400 == 0) || ( (ano % 4 == 0) && (ano % 100 != 0) ) )
+		if( mes==2 && ((ano % 400 == 0) || ( (ano % 4 == 0) && (ano % 100 != 0) )) )
 			bissexto = 1;
 		
 		System.out.println("Dias: " + (dias[mes-1]+bissexto));
