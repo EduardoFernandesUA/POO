5838ea19359eb2e466286e62a908b362ae3e1a08
Your Name <edu.fernandes@ua.pt>
2022-04-05 00:37:48 +0100

    fix

--- POO/src/Aula05/Calendar.java
+++ POO/src/Aula05/Calendar.java
@@ -2,12 +2,12 @@ package Aula05;
 
 public class Calendar {
 	private int year;
-	private int weakDay;
+	private int weekDay;
 
-	Calendar(int year, int weakDay) {
-		assert weakDay>=1 && weakDay<=7;
+	Calendar(int year, int weekDay) {
+		assert weekDay>=1 && weekDay<=7;
 		this.year = year;
-		this.weakDay = weakDay;
+		this.weekDay = weekDay;
 	}
 
 	public int year() {
@@ -15,21 +15,22 @@ public class Calendar {
 	}
 
 	public int firstWeekdayOfYear() {
-		return this.weakDay;
+		return this.weekDay;
 	}
 
 	public int firstWeekdayOfMonth(int month) {
 		assert month>=1 && month<=12;
-		return 0;
-	}
 
-	public String printMonth(int month) {
-		assert month>=1 && month<=12;
 		int totalDays = 0;
 		for (int i = 1; i < month; i++) {
 			totalDays += Date.monthDays(i, this.year);
 		}
-		return makeWeakCalendar(month, (totalDays+this.weakDay-1)%7+1);
+		return (totalDays + this.weekDay-1) % 7 + 1;
+	}
+
+	public String printMonth(int month) {
+		assert month>=1 && month<=12;
+		return makeWeakCalendar(month, firstWeekdayOfMonth(month));
 	}
 
 	public String toString() {
@@ -40,7 +41,7 @@ public class Calendar {
 		return r;
 	}
 
-	private String makeWeakCalendar(int month, int weakDay) {
+	private String makeWeakCalendar(int month, int weekDay) {
 		String[] monthNames = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
 		String r = "\n";
@@ -54,10 +55,10 @@ public class Calendar {
 		r += "Su Mo Tu We Th Fr Sa\n";
 
 		// preenche os dias em branco
-		for(int i = 1; i < weakDay; i++)
+		for(int i = 1; i < weekDay; i++)
 			r += "   ";
 		
-		int x = weakDay-1;
+		int x = weekDay-1;
 		for(int i = 1; i <= Date.monthDays(month, this.year); i++) {
 			r += String.format("%2d ", i);
 			x += 1;
