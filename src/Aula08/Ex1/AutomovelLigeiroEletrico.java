00684002598dd2a4b54c1c5800d96602b0f15b5d
EduardoFernandesUA <edu.fernandes@ua.pt>
2022-05-21 14:31:10 +0100

    .

---
+++ src/Aula08/Ex1/AutomovelLigeiroEletrico.java
@@ -0,0 +1,17 @@
+package Aula08.Ex1;
+
+public class AutomovelLigeiroEletrico implements VeiculoEletrico {
+    private int autonomia;
+    private int charge;
+
+    public int autonomia() {
+        return this.autonomia;
+    }
+
+    public void carregar(int percentagem) {
+        this.charge += percentagem;
+    }
+    public int getCharge() {
+        return this.charge;
+    }
+}
